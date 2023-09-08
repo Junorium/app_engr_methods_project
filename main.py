6da6b813f592bc08cab6f3c5bc8db32f31ed2f84
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2023-09-08 09:45:19 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# https://www.geeksforgeeks.org/how-to-use-chatgpt-api-in-python/#
+'''
+# procedure to getting an API
+pip install -openai
+https://console.cloud.google.com/welcome/new?walkthrough_id=speech-to-text__speech-studio-transcriptions&project=engr010-project
+'''
+
+import openai
+import 
+
+openai.my_api_key = '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'
