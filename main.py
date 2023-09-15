aa67cef6a7ac7d09c2eefe60d00a78298650484e
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2023-09-14 23:31:03 -0400

    Categorize main.py

--- main.py
+++ main.py
@@ -5,7 +5,18 @@ pip install -openai
 https://console.cloud.google.com/welcome/new?walkthrough_id=speech-to-text__speech-studio-transcriptions&project=engr010-project
 '''
 
+# import libraries
 import openai
 import 
 
+# initialize api keys
 openai.my_api_key = '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'
+
+# take inputs
+if button == 1:
+  
+# process
+
+# save outputs
+
+# outputs
