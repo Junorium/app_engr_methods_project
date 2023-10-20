c5de4e6b5881c598bb0357c5fba3416b7ddee639
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2023-10-20 14:04:20 -0400

    Finished draft program

--- processing.py
+++ processing.py
@@ -9,8 +9,35 @@ import openai
 import gpiozero
 import sleep
 
-# initialize api keys
+import os
+import numpy as np
+from gpiozero import Button
+import sounddevice as sd
+
+# initialize api keys; intialize gpio pin
 openai.api_key = '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'
+button = Button(17)
+
+def start_record():
+    # refer to duration as global variable
+    global duration
+    duration = 0
+    output_file = "prompt.wav"
+
+  # copied from documentation
+    def callback(indata, frames, time, status):
+        if status:
+            print(status, file=sys.stderr)
+        if any(indata):
+            # Write audio data to the output file
+            with open(output_file, 'ab') as f:
+                np.savetxt(f, indata, fmt='%f', delimiter=',')
+
+    # begin audio recording
+    with sd.InputStream(callback=callback, channels=1, samplerate=sample_rate):
+        while button.is_active:
+            duration += 1
+            sd.sleep(1000)
 
 def transcribe(audio):
   with open(audio, 'rb'):
@@ -23,6 +50,7 @@ def transcribe(audio):
   )
   
   return response['text']
+
 
 def create_response(prompt):
   response = openai.ChatCompletion.create(
@@ -40,13 +68,31 @@ def main(input, output):
   with open(output, 'w' as file:
     file.write(create_response(input))
 
-if __name__ == "__main__":
-  audio_file_path = '.wav' # audio file
-  transcribed_text = '.txt' # intermediate text file
-  output_response = '.txt' # response from chatgpt
-  
-  with open(transcribed_text, 'w'):
-    transcribed_text.write(transcribe(audio_file_path))
+# change accordingly to GPIO anode of button
 
-  with open(output_response, 'w'):
-    output_response.write(response(transcribed_text))
+
+# audio parameters
+sample_rate = 44100
+duration = 0
+
+def record_close():
+  button.close()
+  sd.stop()
+  sd.close()
+
+def main_create(audio, transcribe, output):
+  with open(transcribe, 'w'):
+    transcribed.write(transcribe(audio))
+
+  with open(output, 'w'):
+    output.write(response(transcribe))
+
+if __name__ == "__main__":
+  audio_file_path = 'prompt.wav' # audio file
+  transcribed_text = 'transcribed.txt' # prompt as text
+  output_response = 'response.txt' # response from chatgpt
+
+  button.when_held = start_record
+  button.when_pressed = main_create(audio_file_path, transcribed_text, output_response)
+  
+  record_close()
