3a93141c5f63eea5beb804c8224a587703092c30
John Richard Perez <46211205+Junorium@users.noreply.github.com>
2023-10-20 10:03:11 -0400

    Update and rename main.py to processing.py

--- processing.py
+++ processing.py
@@ -1,8 +1,7 @@
 # https://www.geeksforgeeks.org/how-to-use-chatgpt-api-in-python/#
 '''
-# procedure to getting an API
 pip install -openai
-https://console.cloud.google.com/welcome/new?walkthrough_id=speech-to-text__speech-studio-transcriptions&project=engr010-project
+pip install gpiozero
 '''
 
 # import libraries
